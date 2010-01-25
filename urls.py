e377ffb6c45d908ff417948b35999d7ead6c5fa5
Chris Beaven <smileychris@gmail.com>
2010-01-25 21:29:44 +1300

    An example of an extended template working.

--- urls.py
+++ urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import patterns
 
 urlpatterns = patterns('repeatblock.views',
     (r'^$', 'repeatblock_example'),
+    (r'^2/$', 'repeatblock_example_2'),
 )
--- views.py
+++ views.py
@@ -5,3 +5,9 @@ def repeatblock_example(request):
     html = template.loader.render_to_string('repeatblock_example.html',
                                             {'title': 'example'})
     return http.HttpResponse(html)
+
+
+def repeatblock_example_2(request):
+    html = template.loader.render_to_string('repeatblock_example_2.html',
+                                            {'title': 'example'})
+    return http.HttpResponse(html)
